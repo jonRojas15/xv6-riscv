7784f5b74aa0ab225dab276b7032d0245037d11a
Jonathan Rojas <rojasjonathan@icloud.com>
2026-09-23 15:24:01 -0600

    Add process CPU time tracking

--- kernel/proc.c
+++ kernel/proc.c
@@ -124,6 +124,8 @@ allocproc(void)
 found:
   p->pid = allocpid();
   p->state = USED;
+  p->cputime = 0;
+
 
   // Allocate a trapframe page.
   if ((p->trapframe = (struct trapframe *)kalloc()) == 0) {
