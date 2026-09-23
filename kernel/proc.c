24e6fefc89a67711d24315d196d77c6e5b728baa
Jonathan Rojas <rojasjonathan@icloud.com>
2026-09-23 16:54:39 -0600

    Implement wait2 system call

--- kernel/proc.c
+++ kernel/proc.c
@@ -5,6 +5,7 @@
 #include "spinlock.h"
 #include "proc.h"
 #include "defs.h"
+#include "pstat.h"
 
 struct cpu cpus[NCPU];
 
@@ -419,6 +420,78 @@ kwait(uint64 addr)
     acquire(&wait_lock);
   }
 }
+
+int
+kwait2(uint64 addr, uint64 raddr)
+{
+  struct proc *pp;
+  int havekids, pid;
+  struct proc *p = myproc();
+
+  acquire(&wait_lock);
+
+  for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+
+    for(pp = proc; pp < &proc[NPROC]; pp++){
+      if(pp->parent == p){
+        acquire(&pp->lock);
+
+        havekids = 1;
+
+        if(pp->state == ZOMBIE){
+          // Found one.
+          pid = pp->pid;
+
+          if(addr != 0 &&
+             copyout(p->pagetable, p->sz, addr,
+                     (char *)&pp->xstate,
+                     sizeof(pp->xstate)) < 0){
+            release(&pp->lock);
+            release(&wait_lock);
+            return -1;
+          }
+
+          if(raddr != 0){
+            struct rusage r;
+            r.cputime = pp->cputime;
+
+            if(copyout(p->pagetable, p->sz, raddr,
+                       (char *)&r,
+                       sizeof(r)) < 0){
+              release(&pp->lock);
+              release(&wait_lock);
+              return -1;
+            }
+          }
+
+          pp->parent = 0;
+          freeproc(pp);
+          release(&pp->lock);
+          release(&wait_lock);
+
+          return pid;
+        }
+
+        release(&pp->lock);
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || killed(p)){
+      release(&wait_lock);
+      return -1;
+    }
+
+    // Wait for a child to exit.
+    sleep_prepare(p);
+    release(&wait_lock);
+    sleep();
+    acquire(&wait_lock);
+  }
+}
+
 
 // Per-CPU process scheduler.
 // Each CPU calls scheduler() after setting itself up.
